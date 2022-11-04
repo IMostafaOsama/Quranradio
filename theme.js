ed490d2f2f0a9c060ab6938d97fd7588959309de
IMostafaOsama <110690109+IMostafaOsama@users.noreply.github.com>
2022-11-04 18:43:54 +0200

    Update theme.js

--- theme.js
+++ theme.js
@@ -4,14 +4,18 @@ const radiostream = document.querySelector(".radiostream").style;
 const btns = document.querySelector("button").style;
 const birds = document.querySelector(".birds").style;
 const cairo = document.querySelector(".cairo").style;
-const clouds = document.querySelector(".clouds").style;
+const saudi = document.querySelector(".saudi").style;
+const marocco = document.querySelector(".marocco").style;
+const emarat = document.querySelector(".emarat").style;
 const stars = document.querySelector(".stars").style;
+const waves = document.querySelector(".waves").style;
+
 const greatingmsg = document.querySelector(".greatingmsg");
 
 setInterval(() => {
   hrs = new Date().toString().split(" ")[4].split(":")[0];
   console.log(hrs);
-  if (hrs >= 6 || hrs <= 18) {
+  if (hrs >= 6 && hrs <= 12) {
     greatingmsg.innerHTML = "صباح الخير";
     greatingmsg.style.color = "black";
     body.backgroundColor = "white";
@@ -21,8 +25,11 @@ setInterval(() => {
     btns.color = "white";
     birds.filter = "brightness(0%)";
     cairo.filter = "brightness(0%)";
+    saudi.filter = "brightness(0%)";
+    marocco.filter = "brightness(0%)";
+    emarat.filter = "brightness(0%)";
+    waves.filter = "brightness(0%)";
     birds.display = "block";
-    clouds.display = "block";
     stars.display = "none";
   } else {
     greatingmsg.innerHTML = "مساء الخير";
@@ -34,8 +41,11 @@ setInterval(() => {
     btns.color = "black";
     birds.filter = "brightness(500%)";
     cairo.filter = "brightness(500%)";
+    saudi.filter = "brightness(500%)";
+    marocco.filter = "brightness(500%)";
+    emarat.filter = "brightness(500%)";
+    waves.filter = "brightness(500%)";
     birds.display = "none";
-    clouds.display = "none";
     stars.display = "block";
   }
 }, 1000);
